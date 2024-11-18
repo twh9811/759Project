46d4418176128ad80f261efb1b59d1c5e4a3ef6b
Travis <twh9811@rit.edu>
2024-11-18 04:09:08 -0500

    Im now only searching for variables and references. Idk wtf I was doing before:

--- docker_analysis.py
+++ docker_analysis.py
@@ -1,26 +1,17 @@
 import yaml
 from yaml.loader import SafeLoader
-
+import re
 
 # These will be used in the "use" section to indicate which action is being used.
-DOCKER_ACTIONS = {
-    # Map docker actions to their taint sources
-    "docker/build-push-action@v6" : {
-        "sources" : ["add-hosts", "allow","annotations","build-args","build-contexts","cache-from","cache-to","context","file","labels","network","platforms","pull","push","secrets","secret-envs","secret-files","ssh","tags","target","github-token"],
-        "sinks": [""]
-        },
-    "docker/login-action@v2" : [], 
-    "docker/setup-buildx-action@v2" : [], 
-    "docker/metadata-action@v4" : [],  
-    "docker/setup-qemu-action@v2" : [],  
-    "docker/buildx-bake-action@v1" : [],  
-    "docker/scout-action@v1" : []
-    }
+DOCKER_ACTIONS = ["docker/build-push-action@v6""docker/login-action@v2", "docker/setup-buildx-action@v2" , "docker/metadata-action@v4",  "docker/setup-qemu-action@v2",  "docker/buildx-bake-action@v1",  "docker/scout-action@v1"]
 
+# Extracts any text between {{ }}
+REFERENCE_PATTERN = "\{\{\s*(.*?)\s*\}\}"
 
 class DockerActionTaintAnalysis:
     def __init__(self, docker_file):
-        self.taint_set = set()
+        self.tainted_placeholders = set()
+        self.tainted_variables = set()
         self.yaml_file = None
         with open(docker_file) as docker_file:
             # Returns a dictionary representing the workflow. Every colon indicates a key, anyhting inside the colon is stored at that key value.
@@ -48,19 +39,24 @@ class DockerActionTaintAnalysis:
         step_name = bulk_steps["name"]
         docker_action = bulk_steps["uses"]
         parameters = bulk_steps["with"]
-        self.process_parameters(docker_action, parameters)
+        self.process_parameters(parameters)
         
-    def process_parameters(self, docker_action, parameters):
+    def process_parameters(self, parameters):
         for parameter in parameters:
-            # Variable most likely tainted?
-            if parameter in DOCKER_ACTIONS[docker_action]["sources"]:
-                self.taint_step(parameters[parameter])
-    
+            parameter_instructions = parameters[parameter]
+            self.process_parameter_instructions(parameter_instructions)
+                
+    def process_parameter_instructions(self, parameter_instructions):
+        # Sometimes the data is stored in a variable, sometimes it is just referenced.
+        # This handles both scenarios.
+        parameter_instructions = parameter_instructions.strip().replace("\n","").split("=")
+        print(parameter_instructions)
+        # split_instructions = parameter_instructions.strip()
+        # print(split_instructions)
 def main():
     docker_file = "test_file.yaml"
     taint_analysis_obj = DockerActionTaintAnalysis(docker_file)
     taint_analysis_obj.process_jobs()
-    print(taint_analysis_obj.taint_set)
     
 if __name__ == "__main__":
     main()
