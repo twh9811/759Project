2f78cd6d3071e7de8d8d7cf64f852cbde4830599
Travis <twh9811@rit.edu>
2024-11-19 00:55:48 -0500

    Ihatethis

--- docker_analysis.py
+++ docker_analysis.py
@@ -20,13 +20,19 @@ class DockerActionTaintAnalysis:
             # Returns a dictionary representing the workflow. Every colon indicates a key, anyhting inside the colon is stored at that key value.
             # There can be dicts inside dicts inside dicts.
             self.yaml_file = yaml.load(docker_file, Loader=SafeLoader)
-    
+            
+        # Make-shift database. More like a cache?
+        self.taint_summaries = {}
+            
     def is_tainted(self, step):
         return step in self.taint_set
     
     def taint_variable(self, variable):
         self.tainted_variables.add(variable)
     
+    def found_taints(self, workflow_string):
+        taints = re.findall(REFERENCE_PATTERN, workflow_string)
+        return len(taints) != 0
     
     def process_workflow(self):
         for item in self.yaml_file:
@@ -44,16 +50,38 @@ class DockerActionTaintAnalysis:
             
     def process_jobs(self, job_workflow):
         for job_name in job_workflow:
-            print(job_name)
-            #print(job_workflow[job_name]["steps"])
+            job_steps = job_workflow[job_name]["steps"]
+            self.process_bulk_steps(job_steps)
+                    
+    def process_bulk_steps(self, job_steps):
+        # This will have ALL the steps of the job as dictionaries
+        for bulk_steps in job_steps:
+            self.process_individual_steps(bulk_steps)
             
-                
+    def process_individual_steps(self, bulk_steps):
+        step_name = bulk_steps["name"]
+        docker_action = bulk_steps["uses"]
+        
+        # If summary exists for docker action, use it
+        if docker_action in self.taint_summaries.keys():
+            pass
+        # Otherwise perform further processing    
+        else:
+            if "with" in bulk_steps:
+                parameters = bulk_steps["with"]
+                self.process_parameters(parameters)
+            
+    def process_parameters(self, parameters):
+        for parameter in parameters:
+            parameter_instructions = str(parameters[parameter])
+            if(self.found_taints(parameter_instructions)):
+                self.taint_variable(parameter)
             
 def main():
     docker_file = "test_file.yaml"
     taint_analysis_obj = DockerActionTaintAnalysis(docker_file)
     taint_analysis_obj.process_workflow()
-    #print(taint_analysis_obj.tainted_variables)
+    print(taint_analysis_obj.tainted_variables)
     # taint_analysis_obj.process_jobs()
     # print(taint_analysis_obj.tainted_variables)
     
