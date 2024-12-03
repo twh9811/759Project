1a21252c6a857e13cff1d7a8c1818a52d7e6a51d
Travis <twh9811@rit.edu>
2024-12-02 23:19:13 -0500

    added file writing support

--- src/main.py
+++ src/main.py
@@ -3,8 +3,29 @@ import generate_action_summary
 import taint_analysis
 BASE_DIR = "src/example/"
 
-def write_to_file():
-    pass
+def write_to_file(filepath, tainted_vars, tainted_flow):
+    """
+    Writes the summary to a file. Rewrites the file.
+
+    Args:
+        filepath (string): Path to the file
+        tainted_vars (list): list of tainted variables
+        tainted_flow (ldictionary): dict containing the taint flows 
+    """
+    with open(filepath, "w") as file:
+        file.write("All Tainted Variables:\n\n")
+        for var in tainted_vars:
+            file.write(var + "\n")
+
+        file.write("\nTainted Flows:\n")
+        num = 0
+        for origin in tainted_flow:
+            file.write("\tFlow " + str(num) + ":\n")
+            flow = tainted_flow[origin]
+            file.write("\t\tOrigin: " + origin + "\n")
+            for affected_var in flow:
+                file.write("\t\t\tPropagation: " + affected_var + "\n")
+            num += 1
     
 def main():
     main_workflow = BASE_DIR + "sample-workflow.yaml"
@@ -19,6 +40,9 @@ def main():
     # Gets the flow summary and tainted variable summary from the taint analysis
     tainted_vars, tainted_flow = taint_analysis_summary.get_tainted_variables()
     
+    filename = "results/summary.txt"
+    write_to_file(filename, tainted_vars, tainted_flow)
+    
     print("All Tainted Variables:")
     for var in tainted_vars:
         print(var)
@@ -29,9 +53,9 @@ def main():
     for origin in tainted_flow:
         print("Flow " + str(num) + ":")
         flow = tainted_flow[origin]
-        print(" " + origin)
+        print(" Origin: " + origin)
         for affected_var in flow:
-            print(" " + affected_var)
+            print("     Propagation: " + affected_var)
         num += 1
         print()
     
